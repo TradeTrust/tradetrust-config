99c14ed535380500ebbeff165485071acb69e089
Sim Boon Long <simboonlong@gmail.com>
2022-02-23 09:44:06 +0800

    feat: allow local origin (#18)

--- netlify/functions/schema/index.ts
+++ netlify/functions/schema/index.ts
@@ -10,11 +10,17 @@ export const handler: Handler = async (event) => {
   const schema = version === "3.0" ? schemaV3 : schemaV2;
   let headers;
 
-  if (ALLOWED_ORIGINS.includes(origin)) {
+  if (process.env.NETLIFY_LOCAL) {
     headers = {
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": origin,
       "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Origin": "*",
+    };
+  } else if (ALLOWED_ORIGINS.includes(origin)) {
+    headers = {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Origin": origin,
     };
   } else {
     headers = {
