8817805dd020dd37894ecbea0b2043dfde6462cf
Sim Boon Long <simboonlong@gmail.com>
2023-07-25 15:40:32 +0800

    test: dns txt

--- src/generate-config.ts
+++ src/generate-config.ts
@@ -6,41 +6,10 @@ import { configFileV2, configFileV3 } from "./examples/config-file";
 import { walletLocal, walletSample } from "./examples/wallet";
 import { getUpdatedConfigV2, getUpdatedConfigV3 } from "./helpers/helpers";
 import { ConfigFileWithFormV2, ConfigFileWithFormV3 } from "./types";
-import {
-  CHAIN_ID,
-  SUPPORTED_CHAINS,
-} from "@govtechsg/tradetrust-utils/constants/supportedChains";
+import { SUPPORTED_CHAINS } from "@govtechsg/tradetrust-utils/constants/supportedChains";
 import { validateConfig } from "./utils/utils";
-
+import { buildData } from "./constants";
 const DIR = path.join(__dirname, "../build");
-
-// addresses exists as txt-records in respective domains
-const buildData = [
-  {
-    chainId: "80001" as CHAIN_ID,
-    documentStoreAddress: "0x93092C2B449712281008112870063fF439367C00",
-    tokenRegistryAddress: "0x072FB36B73a7f52A23ea53162583f78ba3Bc6DEa",
-    dnsVerifiable: "example.tradetrust.io",
-    dnsTransferableRecord: "example.tradetrust.io",
-    dnsDid: "example.tradetrust.io",
-  },
-  {
-    chainId: "11155111" as CHAIN_ID,
-    documentStoreAddress: "0x71D28767662cB233F887aD2Bb65d048d760bA694",
-    tokenRegistryAddress: "0x142Ca30e3b78A840a82192529cA047ED759a6F7e",
-    dnsVerifiable: "example.tradetrust.io",
-    dnsTransferableRecord: "example.tradetrust.io",
-    dnsDid: "example.tradetrust.io",
-  },
-  {
-    chainId: "1337" as CHAIN_ID, // local network will skip dns verifier
-    documentStoreAddress: "0x63a223e025256790e88778a01f480eba77731d04",
-    tokenRegistryAddress: "0x9Eb613a88534E2939518f4ffBFE65F5969b491FF",
-    dnsVerifiable: "example.com",
-    dnsTransferableRecord: "example.com",
-    dnsDid: "example.com",
-  },
-];
 
 const writeConfigFile = (
   configFile: ConfigFileWithFormV2 | ConfigFileWithFormV3,
---
+++ test/dns.test.ts
@@ -0,0 +1,51 @@
+import fetch from "cross-fetch";
+import { buildData } from "../src/constants";
+
+const testNetworks = buildData.filter((data) => data.chainId !== "1337"); // we omit checking for 1337 local
+
+const resolveDns = async (domainName: string) => {
+  try {
+    const response = await fetch(
+      `https://dns.google/resolve?name=${domainName}&type=TXT`,
+    );
+    const data = await response.json();
+
+    if (data.Status === 0) {
+      return data.Answer;
+    } else {
+      console.log(`Unable to resolve IP address for ${domainName}`);
+    }
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
+};
+
+describe("dns text", () => {
+  test("should contain respective verifiable txt-records", async () => {
+    const found: boolean[] = [];
+
+    for (let i = 0; i < testNetworks.length; i++) {
+      const answers = await resolveDns(testNetworks[i].dnsVerifiable);
+      const isFound = answers.some((answer: { data: string | string[] }) =>
+        answer.data.includes(testNetworks[i].documentStoreAddress),
+      );
+      found.push(isFound);
+    }
+
+    expect(found.every((bool) => bool === true)).toBe(true);
+  });
+
+  test("should contain respective transferable txt-records", async () => {
+    const found: boolean[] = [];
+
+    for (let i = 0; i < testNetworks.length; i++) {
+      const answers = await resolveDns(testNetworks[i].dnsTransferableRecord);
+      const isFound = answers.some((answer: { data: string | string[] }) =>
+        answer.data.includes(testNetworks[i].tokenRegistryAddress),
+      );
+      found.push(isFound);
+    }
+
+    expect(found.every((bool) => bool === true)).toBe(true);
+  });
+});
