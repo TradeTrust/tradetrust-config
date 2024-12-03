cd13f1433eeda6bc9f92fab8aa368c3b95f81a31
nghaninn <43451336+nghaninn@users.noreply.github.com>
2024-12-03 10:27:19 +0800

    feat: v5 token registry

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ export const buildData = [
   {
     chainId: "20180427" as CHAIN_ID,
     documentStoreAddress: "0x71D28767662cB233F887aD2Bb65d048d760bA694",
-    tokenRegistryAddress: "0xA594f6e10564e87888425c7CC3910FE1c800aB0B",
+    tokenRegistryAddress: "0x1DC4C199DC3694fD5a46aB0C01403469192eD2dc",
     dnsVerifiable: "example.tradetrust.io",
     dnsTransferableRecord: "example.tradetrust.io",
     dnsDid: "example.tradetrust.io",
@@ -15,7 +15,7 @@ export const buildData = [
   {
     chainId: "101010" as CHAIN_ID,
     documentStoreAddress: "0xA594f6e10564e87888425c7CC3910FE1c800aB0B",
-    tokenRegistryAddress: "0x71D28767662cB233F887aD2Bb65d048d760bA694",
+    tokenRegistryAddress: "0x29C67f08994390c1b9786f1F7E1Ed15CB0b391e7",
     dnsVerifiable: "example.tradetrust.io",
     dnsTransferableRecord: "example.tradetrust.io",
     dnsDid: "example.tradetrust.io",
@@ -24,7 +24,7 @@ export const buildData = [
   {
     chainId: "51" as CHAIN_ID,
     documentStoreAddress: "0x93092C2B449712281008112870063fF439367C00",
-    tokenRegistryAddress: "0x71D28767662cB233F887aD2Bb65d048d760bA694",
+    tokenRegistryAddress: "0x9066C02cAf7A60a905b094Fb8c773441f5693569",
     dnsVerifiable: "example.tradetrust.io",
     dnsTransferableRecord: "example.tradetrust.io",
     dnsDid: "example.tradetrust.io",
@@ -33,7 +33,7 @@ export const buildData = [
   {
     chainId: "11155111" as CHAIN_ID,
     documentStoreAddress: "0x71D28767662cB233F887aD2Bb65d048d760bA694",
-    tokenRegistryAddress: "0x142Ca30e3b78A840a82192529cA047ED759a6F7e",
+    tokenRegistryAddress: "0x8280a08a3D6C8567e5Ad867F3c4d36F286767606",
     dnsVerifiable: "example.tradetrust.io",
     dnsTransferableRecord: "example.tradetrust.io",
     dnsDid: "example.tradetrust.io",
@@ -51,7 +51,7 @@ export const buildData = [
   {
     chainId: "296" as CHAIN_ID,
     documentStoreAddress: "0x93092C2B449712281008112870063fF439367C00",
-    tokenRegistryAddress: "0xA594f6e10564e87888425c7CC3910FE1c800aB0B",
+    tokenRegistryAddress: "",
     dnsVerifiable: "example.tradetrust.io",
     dnsTransferableRecord: "example.tradetrust.io",
     dnsDid: "example.tradetrust.io",
@@ -60,7 +60,7 @@ export const buildData = [
   {
     chainId: "80002" as CHAIN_ID,
     documentStoreAddress: "0x93092C2B449712281008112870063fF439367C00",
-    tokenRegistryAddress: "0x71D28767662cB233F887aD2Bb65d048d760bA694",
+    tokenRegistryAddress: "0x3652EfBc80aCE560844AFC932D2bf8b452A96c6d",
     dnsVerifiable: "example.tradetrust.io",
     dnsTransferableRecord: "example.tradetrust.io",
     dnsDid: "example.tradetrust.io",
@@ -69,7 +69,7 @@ export const buildData = [
   {
     chainId: "1338" as CHAIN_ID,
     documentStoreAddress: "0xA594f6e10564e87888425c7CC3910FE1c800aB0B",
-    tokenRegistryAddress: "0xF717d93C751F1835078B513275B14121798C7740",
+    tokenRegistryAddress: "",
     dnsVerifiable: "example.tradetrust.io",
     dnsTransferableRecord: "example.tradetrust.io",
     dnsDid: "example.tradetrust.io",
